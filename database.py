71ed18174ff53295b16246107e24a8dc88338363
nyaswabuhh <alex.navitech@gmail.com>
2025-03-03 20:30:22 +0300

    techcamp-fix

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-conn = psycopg2.connect(host = "134.209.24.19", user = "nyaswabu" , database ="nyaswabu", password ="12345" )
+conn = psycopg2.connect(host = "134.209.24.19", user = "alex" , database ="alex", password ="12345" )
 
 cur = conn.cursor()
 
